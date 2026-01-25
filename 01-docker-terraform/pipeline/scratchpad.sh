2432659ea4bc4eab727f03bba5eb6bd744965cc0
Pavel Garanin <pavelmg93@gmail.com>
2026-01-25 19:41:00 +0000

    scratchpad updated

--- 01-docker-terraform/pipeline/scratchpad.sh
+++ 01-docker-terraform/pipeline/scratchpad.sh
@@ -1,6 +1,7 @@
-scratchpad
+# Scratchpad
+
 # Run PostgreSQL on the network
-docker run -it \
+docker run -it --rm \
   -e POSTGRES_USER="root" \
   -e POSTGRES_PASSWORD="root" \
   -e POSTGRES_DB="ny_taxi" \
@@ -11,7 +12,7 @@ docker run -it \
   postgres:18
 
 # In another terminal, run pgAdmin on the same network
-docker run -it \
+docker run -it --rm \
   -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
   -e PGADMIN_DEFAULT_PASSWORD="root" \
   -v pgadmin_data:/var/lib/pgadmin \
@@ -23,9 +24,10 @@ docker run -it \
 # Dockerizing data ingestion script
 docker build -t taxi_ingest:v001 .
 
-docker run -it \
---network=pg-network \
-taxi_ingest:v001 \
+docker run -it --rm \
+  --network=pg-network \
+  --name pipeline \
+  taxi_ingest:v001 \
   --pg-user=root \
   --pg-pass=root \
   --pg-host=pgdatabase \
@@ -36,14 +38,64 @@ taxi_ingest:v001 \
   --month=2 \
   --chunksize=100000
 
-docker run -it \
-  -t taxi_ingest:v001 \
+# Zones
+https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv
+
+# modified Dockerfile temporarily to use ingest_data_zones.py
+docker build -t zones_ingest .
+
+docker run -it --rm \
+  --network=pipeline_default \
+  zones_ingest \
   --pg-user=root \
   --pg-pass=root \
   --pg-host=pgdatabase \
   --pg-port=5432 \
   --pg-db=ny_taxi \
-  --target-table=yellow_taxi_trips_2021_2 \
-  --year=2021 \
-  --month=2 \
+  --target-table=zones \
   --chunksize=100000
+
+# GCP Deployment
+
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg curl -y
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install google-cloud-cli -y
+
+# Terraform install method 1
+
+# Install dependencies
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+# Add HashiCorp GPG key
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+# Add official repository
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Install Terraform
+sudo apt-get update && sudo apt-get install terraform
+
+# Verify
+terraform -version
+
+
+# Terraform install method 2
+
+# Download the 64-bit Linux binary
+wget https://releases.hashicorp.com/terraform/1.10.5/terraform_1.10.5_linux_amd64.zip
+
+# Install unzip if you don't have it
+sudo apt-get install -y unzip
+
+# Unzip and move to your path
+unzip terraform_1.10.5_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+
+# Verify it works
+terraform -version

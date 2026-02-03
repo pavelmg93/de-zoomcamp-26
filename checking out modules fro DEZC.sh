972413405864e8de22eec2c853e2c1a569bd7d53
Pavel Garanin <pavelmg93@gmail.com>
2026-02-03 02:54:34 +0000

    git module checkout commands and setup.sh

--- checking out modules fro DEZC.sh
+++ checking out modules fro DEZC.sh
@@ -1,5 +1,29 @@
+git merge --abort
+
 git remote add upstream https://github.com/DataTalksClub/data-engineering-zoomcamp.git
 git sparse-checkout init --cone
-git sparse-checkout set 02-workflow-orchestration
+
+# step 1
+git sparse-checkout set 03-data-warehouse
+# step 2
+git checkout upstream/main -- 03-data-warehouse
+
 git fetch upstream
 git pull upstream main --allow-unrelated-histories
+
+
+# -----------
+# (Only if not already added)
+git remote add upstream https://github.com/DataTalksClub/data-engineering-zoomcamp.git 
+
+git fetch upstream
+
+git checkout upstream/main -- 03-data-warehouse
+
+git add 03-data-warehouse
+
+git commit -m "Add module 03 files"
+
+git push
+
+git sparse-checkout disable
